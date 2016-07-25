1118765949b00da19555fb79e3be6f82abc9acf9
Echogogo <zoumo523@gmail.com>
2016-07-24 21:33:35 -0400

    update view

--- assignment2/assignment2/App_Start/BundleConfig.cs
+++ assignment2/assignment2/App_Start/BundleConfig.cs
@@ -25,6 +25,7 @@ namespace assignment2
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
